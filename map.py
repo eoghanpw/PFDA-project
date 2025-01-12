3391a8f83e04125a504d861ac9e16068c0223e84
Eoghan Walsh <eoghanpwalsh@gmail.com>
2025-01-12 23:12:55 +0000

    working on project

--- map.py
+++ map.py
@@ -8,53 +8,56 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 from shapely.geometry import Point
 
-# Ireland map coordinates.
-file_path = "./data/Counties___OSi_National_Statutory_Boundaries___Generalised_20m_-6920972630406172930.geojson"
 
-# Import to geopandas dataframe.
-# https://geopandas.org/en/latest/docs/user_guide/fiona_to_pyogrio.html#migration-from-the-fiona-to-the-pyogrio-read-write-engine
-ireland = gpd.read_file(file_path, engine="pyogrio")
+def ireland_map():
 
-# Weather station coordinates.
-station_details = "./data/met_eireann_station_details.csv"
+    # Ireland map coordinates.
+    file_path = "./data/Counties___OSi_National_Statutory_Boundaries___Generalised_20m_-6920972630406172930.geojson"
 
-# Import to dataframe.
-stations = pd.read_csv(station_details)
+    # Import to geopandas dataframe.
+    # https://geopandas.org/en/latest/docs/user_guide/fiona_to_pyogrio.html#migration-from-the-fiona-to-the-pyogrio-read-write-engine
+    ireland = gpd.read_file(file_path, engine="pyogrio")
 
-# Add coordinates to dataframe.
-# https://datascience.quantecon.org/tools/maps.html#id1
-stations["coordinates"] = list(zip(stations.longitude, stations.latitude))
+    # Weather station coordinates.
+    station_details = "./data/met_eireann_station_details.csv"
 
-stations["coordinates"] = stations["coordinates"].apply(Point)
+    # Import to dataframe.
+    stations = pd.read_csv(station_details)
 
-# Create geopandas dataframe for stations.
-station_df = gpd.GeoDataFrame(stations, geometry="coordinates")
+    # Add coordinates to dataframe.
+    # https://datascience.quantecon.org/tools/maps.html#id1
+    stations["coordinates"] = list(zip(stations.longitude, stations.latitude))
 
-# Ensure station coordinates line up with ireland map when plotting.
-# https://geopandas.org/en/stable/docs/reference/api/geopandas.GeoDataFrame.set_crs.html
-station_df = station_df.set_crs(4326, allow_override=True)
+    stations["coordinates"] = stations["coordinates"].apply(Point)
 
-# Plot the map.
-# https://geopandas.org/en/stable/docs/user_guide/mapping.html#maps-with-layers
-fig, gax = plt.subplots(figsize=(10, 10))
+    # Create geopandas dataframe for stations.
+    station_df = gpd.GeoDataFrame(stations, geometry="coordinates")
 
-base = ireland.plot(ax=gax, edgecolor='gray', color='green', alpha=0.5)
+    # Ensure station coordinates line up with ireland map when plotting.
+    # https://geopandas.org/en/stable/docs/reference/api/geopandas.GeoDataFrame.set_crs.html
+    station_df = station_df.set_crs(4326, allow_override=True)
 
-station_df.plot(ax=base, marker='*', color='red')
+    # Plot the map.
+    # https://geopandas.org/en/stable/docs/user_guide/mapping.html#maps-with-layers
+    gax = plt.subplots()
 
-gax.set_title('Met Eireann Synoptic Weather Stations')
+    base = ireland.plot(ax=gax, edgecolor='gray', color='green', alpha=0.5)
 
-# Remove spines and axes.
-gax.spines['top'].set_visible(False)
-gax.spines['bottom'].set_visible(False)
-gax.spines['right'].set_visible(False)
-gax.spines['left'].set_visible(False)
+    station_df.plot(ax=base, marker='*', color='red')
 
-plt.gca().set_xticks([])
-plt.gca().set_yticks([])
+    gax.set_title('Met Eireann Synoptic Weather Stations')
 
-# Label the stations.
-for x, y, label in zip(station_df['coordinates'].x, station_df['coordinates'].y, station_df['name']):
-    gax.annotate(label, xy=(x, y), xytext=(4, -2), textcoords='offset points', size='x-small', weight='bold')
+    # Remove spines and axes.
+    gax.spines['top'].set_visible(False)
+    gax.spines['bottom'].set_visible(False)
+    gax.spines['right'].set_visible(False)
+    gax.spines['left'].set_visible(False)
 
-plt.show()
+    plt.gca().set_xticks([])
+    plt.gca().set_yticks([])
+
+    # Label the stations.
+    for x, y, label in zip(station_df['coordinates'].x, station_df['coordinates'].y, station_df['name']):
+        gax.annotate(label, xy=(x, y), xytext=(4, -2), textcoords='offset points', size='x-small', weight='bold')
+
+    return gax
